ed6a73baa64c2cfb4ee2bbdf96a8e146ce037bca
Kririna <kiraonnea@gmail.com>
2021-08-11 17:03:06 +0300

    modified src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int bill = 100;
-        int transfer_amount = 1100;
+        int transferAmount = 1100;
         int bonus = 100;
-        int total_bill = (transfer_amount > 1000) ? bill + transfer_amount + transfer_amount / bonus : bill + transfer_amount;
-        System.out.println(total_bill);
+        int totalBill = (transferAmount > 1000) ? bill + transferAmount + transferAmount / bonus : bill + transferAmount;
+        System.out.println(totalBill);
     }
 }
